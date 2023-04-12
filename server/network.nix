7a0205d75107289330e844f2d2a93e583b7b52d0
Philipp Gaßner <rillo@cyberpsych0si.is>
2023-04-12 02:54:20 +0200

    added network config

--- server/network.nix
+++ server/network.nix
@@ -17,5 +17,51 @@
   # On WAN, allow IPv6 autoconfiguration and tempory address use.
   #"net.ipv6.conf.${name}.accept_ra" = 2;
   #"net.ipv6.conf.${name}.autoconf" = 1;
+
+  networking = {
+  useDHCP = false;
+  hostName = "router";
+  nameserver = [ 172.16.0.1 ];
+  # Define VLANS
+  vlans = {
+    main = {
+      id = 10;
+      interface = "enp2s0";
+    };
+    test = {
+      id = 30;
+      interface = "enp2s0";
+    };
+  };
+
+
+
+  interfaces = {
+    # Don't request DHCP on the physical interfaces
+    enp1s0.useDHCP = false;
+    enp1s0.ipv4.addresses = [ {
+      address = 172.16.0.2;
+      prefixLength = 23;
+    }];
+
+
+    enp2s0.useDHCP = false;
+    
+    # Handle the VLANs
+    wan.useDHCP = false;
+    main = {
+      ipv4.addresses = [{
+        address = "10.0.16.1";
+        prefixLength = 20;
+      }];
+    };
+    iot = {
+      ipv4.addresses = [{
+        address = "10.0.24.1";
+        prefixLength = 20;
+      }];
+    };
+  };
+};
 };
 }
