89febb9bebec5f0249706683cd89d83e9977cf00
Philipp Gaßner <rillo@cyberpsych0si.is>
2023-04-12 17:19:34 +0200

    :(

--- server/network.nix
+++ server/network.nix
@@ -23,15 +23,16 @@
     enable = true;
     interfaces = [ "main@enp2s0" ];
     extraConfig = ''
+      option domain-name-servers 9.9.9.9;
+      option subnet-mask 255.255.240.0;
+      option broadcast-address 10.0.23.255;
+      option routers 10.0.16.1;
+
       subnet 10.0.16.0 netmask 255.255.240.0 {
-        range 10.0.16.20 10.0.31.255;
+        range 10.0.16.20 10.0.16.200;
         interface main@enp2s0;
         default-lease-time 3600;
         max-lease-time 7200;
-        option domain-name-servers 9.9.9.9;
-        option subnet-mask 255.255.240.0;
-        option broadcast-address 10.0.23.255;
-        option routers 10.0.16.1;
       }
     '';
   };
