c358fdd4460dae9555b591b15c7a71aa6c9ca344
Philipp Gaßner <rillo@cyberpsych0si.is>
2023-04-12 18:41:50 +0200

    added samba

--- server/configuration.nix
+++ server/configuration.nix
@@ -10,6 +10,7 @@
       # Include the results of the hardware scan.
       /etc/nixos/hardware-configuration.nix
       ./network.nix
+      ./samba.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
