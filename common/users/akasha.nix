534389ad183b1b863e2db46c2b2426e64eeae94c
Philipp Gaßner <rillo@cyberpsych0si.is>
2023-04-15 19:46:50 +0200

    akasha ugroup

--- common/users/akasha.nix
+++ common/users/akasha.nix
@@ -3,11 +3,13 @@
   users.users.akasha = {
     isNormalUser = true;
     description = "akasha";
-    extraGroups = [ "networkmanager" "wheel" "smbusers" ];
+    extraGroups = [ "networkmanager" "wheel" "smbusers" "akasha"];
     packages = with pkgs; [
       firefox
       #  thunderbird
     ];
   };
+
+    users.groups.akasha = {};
 
 }
