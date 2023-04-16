412bdeac865f769f9180af9d4b395f64144c7acc
Philipp Gaßner <rillo@cyberpsych0si.is>
2023-04-16 16:15:52 +0200

    dhcp

--- server/services/dhcp.nix
+++ server/services/dhcp.nix
@@ -10,11 +10,12 @@
         interface main;
         default-lease-time 3600;
         max-lease-time 7200;
-        option domain-name-servers 10.0.16.1;
         option subnet-mask 255.255.240.0;
         option broadcast-address 10.0.23.255;
         option routers 10.0.16.1;
-        option domain-search main.rillonautikum.internal;
+        option domain-name "main.rillonautikum.internal";
+        option domain-search "main.rillonautikum.internal";
+        option domain-name-servers 10.0.16.1;
       }
     '';
   };
