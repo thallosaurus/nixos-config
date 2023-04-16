ecd174cfa483bbb6f173273fe20e83a201e2cd08
Philipp Gaßner <rillo@cyberpsych0si.is>
2023-04-16 17:26:05 +0200

    added tsig to dhcp

--- server/services/dhcp.nix
+++ server/services/dhcp.nix
@@ -5,6 +5,17 @@
     enable = true;
     interfaces = [ "main" ];
     extraConfig = ''
+      include "/mnt/certs/tsig.key";
+
+      ddns-updates on;
+      ddns-update-style interim;
+      update-static-leases on;
+
+      zone main.rillonautikum.internal. {
+        primary 127.0.0.1;
+        key tsig-key;
+      }
+
       subnet 10.0.16.0 netmask 255.255.240.0 {
         range 10.0.16.20 10.0.16.200;
         interface main;
