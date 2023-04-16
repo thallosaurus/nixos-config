472b9ecfeb131fd5e17cd7f709ee42d90cb279a8
Philipp Gaßner <rillo@cyberpsych0si.is>
2023-04-16 14:17:59 +0200

    dhcp

--- server/services/dhcp.nix
+++ server/services/dhcp.nix
@@ -10,7 +10,7 @@
         interface main;
         default-lease-time 3600;
         max-lease-time 7200;
-        option domain-name-servers 9.9.9.9;
+        option domain-name-servers 10.0.16.1;
         option subnet-mask 255.255.240.0;
         option broadcast-address 10.0.23.255;
         option routers 10.0.16.1;
